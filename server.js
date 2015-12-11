8a25ca7c8057eb1d7e553362357eda25f98e2d67
Geoffrey Tsai <geoff.tsai@williamhill.com.au>
2015-12-11 17:01:16 +1100

    rewrite the consumer monitor so that it loads the data on start up and then will poll kafka every 10 seconds for data

--- server.js
+++ server.js
@@ -1,125 +1,36 @@
-var args = require('yargs').argv,
-    zk = require('node-zookeeper-client'),
-    kafka = require('kafka-node'),
-    Table = require('cli-table'),
-    linq = require('node-linq').LINQ,
-    express = require('express');
+var Table = require('cli-table');
+var linq = require('node-linq').LINQ;
+var express = require('express');
 
-if (!args.zkConnect && !process.env.ZOOKEEPER_CONNECT) {
-    console.error('zkConnect not specified');
-    return;
-}
+var zkLib = require('./lib/ZooKeeper.js');
+var kafkaLib = require('./lib/Kafka.js');
+var cache    = require('./lib/Cache.js').cache;
 
 var app = express();
 
 
-var getZkConnect = function() {
-    var connect = args.zkConnect || process.env.ZOOKEEPER_CONNECT;
-    return connect;
+zkLib.loadConsumerMetaData(function() {
+    console.log('loading the consumer offsets');
+    kafkaLib.getTopicOffsets();
+});
+
+var pollKafkaOffsets = function() {
+    setTimeout(function() {
+        kafkaLib.getTopicOffsets(pollKafkaOffsets);
+    }, 10000);
 };
 
+pollKafkaOffsets();
 
 app.get('/consumers/:consumer/lag', function (req, res) {
-    var consumer = req.params.consumer,
-        zkClient = zk.createClient(getZkConnect()),
-        kafkaClient = new kafka.Client(getZkConnect(), 'lagger'),
-        kafkaOffset = new kafka.Offset(kafkaClient),
-
-        denodeify = function (f, that) {
-            return function () {
-                var args = Array.prototype.slice.call(arguments);
-                return new Promise(function (resolve, reject) {
-                    f.apply(that, args.concat(function (e, d, s) {
-                        if (e) return reject(e);
-                        return resolve({ d: d, s: s, args: args });
-                    }));
-                });
-            };
-        },
-
-        getChildren = denodeify(zkClient.getChildren, zkClient),
-        getData = denodeify(zkClient.getData, zkClient),
-        remove = denodeify(zkClient.remove, zkClient),
-        close = denodeify(zkClient.close, zkClient);
-
-        get = function () {
-            return getChildren('/consumers/' + consumer + '/offsets')
-                .then(function (topicsResult) {
-                    var topics = topicsResult.d;
-                    return Promise.all(topics.map(function (t) {
-                        return getChildren(topicsResult.args[0] + '/' + t)
-                            .then(function (partitionsResult) {
-                                var partitions = partitionsResult.d;
-                                return partitions.map(function (p) {
-                                    return {
-                                        consumer: consumer,
-                                        topic: t,
-                                        partition: p
-                                    };
-                                });
-                            });
-                    }));
-                })
-                .then(function (topicPartitions) {
-                    var partitions = [];
-                    topicPartitions.forEach(function (tp) {
-                        partitions = partitions.concat(tp);
-                        return;
-                    });
-                    return Promise.all(partitions.map(function (p) {
-                        return getData('/consumers/' + p.consumer + '/offsets/' + p.topic + '/' + p.partition)
-                            .then(function (offsetResult) {
-                                p.offset = offsetResult.d.toString();
-                                return p;
-                            })
-                            .then(function (partition) {
-                                var t = partition.topic,
-                                    p = partition.partition;
-                                return new Promise(function (resolve, reject) {
-                                    kafkaOffset.fetch([{
-                                        topic: t,
-                                        partition: p,
-                                        time: -1,
-                                        maxNum: 1
-                                    }], function (e, d) {
-                                        if (e) {
-                                            partition.end = -1;
-                                            partition.lag = -1;
-                                            return resolve(partition);
-                                        }
-
-                                        var value = d[t][p];
-                                        if (value.length) value = value[0];
-                                        else value = undefined;
-
-                                        partition.end = value;
-                                        partition.lag = value - partition.offset;
-
-                                        resolve(partition);
-                                    });
-                                });
-                            });
-                    }));
-                })
-                .then(function (r) {
-                    var result = new linq(r)
-                        .OrderBy(function(r) {
-                            return r.consumer + '__' + r.topic + '__' + r.partition;
-                        })
-                        .ToArray();
-                    res.send(r);
-                })
-                .catch(function (e) {
-                    res.status(500).send({ error: e });
-                });
-        };
-
-        zkClient.once('connected', function () {
-            get();
-        });
-
-    zkClient.connect();
-
+    cache.get(req.params.consumer, function(err, value){
+        if (!err) {
+            res.send(value);
+        }
+        else {
+            res.status(500).send();
+        }
+    });
 });
 
 app.listen(8000);
