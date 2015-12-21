ae46b4121f7f1c03dcd53f876665a0d243f4a6fa
Geoffrey Tsai <geoff.tsai@williamhill.com.au>
2015-12-22 10:53:50 +1100

    implement the consumer monitor refresh

--- server.js
+++ server.js
@@ -1,30 +1,49 @@
-var Table       = require('cli-table');
-var linq        = require('node-linq').LINQ;
-var express     = require('express');
-
 var zkLib       = require('./lib/ZooKeeper.js');
 var kafkaLib    = require('./lib/Kafka.js');
 var cache       = require('./lib/Cache.js').cache;
 var logger      = require('./logger.js').logger;
 var config      = require('./config');
-
-var app = express();
-
-
-zkLib.loadConsumerMetaData(function() {
-    logger.trace('loading the consumer offsets');
-    kafkaLib.getTopicOffsets();
-});
-
-var pollKafkaOffsets = function() {
-    setTimeout(function() {
-        kafkaLib.getTopicOffsets(pollKafkaOffsets);
-    }, config.refreshInterval);
+var express     = require('express');
+var app         = express();
+var loadMetadata = function(callback) {
+    zkLib.loadConsumerMetaData(callback);
 };
 
-pollKafkaOffsets();
+var pollKafkaOffsets = function() {
+    logger.trace('polling for the latest kafka offsets');
+    setTimeout(function() {
+        kafkaLib.getTopicOffsets(pollKafkaOffsets);
+    }, config.refreshInterval.lag);
+};
 
-app.get('/consumers/:consumer/lag', function (req, res) {
+var pollMetadata = function() {
+    logger.trace('polling the latest metadata');
+    setTimeout(function() {
+        loadMetadata(pollMetadata);
+    }, config.refreshInterval.metadata);
+};
+
+
+loadMetadata(function() {
+    pollMetadata();
+    loadKafkaOffsets(pollKafkaOffsets);
+});
+
+
+app.get('/monitor/refresh', function(req, res) {
+    logger.trace('load metadata called from external source');
+    loadMetadata(function(err){
+        if (err) {
+            return res.status(500).json({'error_code': 500, 'message': err});
+        }
+
+        logger.trace('completed loading metadata');
+        res.status(200).send();
+    });
+});
+
+
+app.get('/consumers/:consumer/lag', function(req, res) {
     cache.get(req.params.consumer, function(err, value){
         if (!err) {
             // TODO : sort the response here if required
@@ -37,4 +56,4 @@ app.get('/consumers/:consumer/lag', function (req, res) {
     });
 });
 
-app.listen(8000);
+app.listen(config.server.port);
